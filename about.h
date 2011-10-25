f6bdb1847b1395375e5a831385890bd57f2f1043
Bartłomiej Cieślak <bartlomiej@cieslak.info.pl>
2011-10-25 14:10:50 +0200

    Dodałem reakcje na kliknięcie w linki

--- about.h
+++ about.h
@@ -15,6 +15,13 @@ public:
     explicit About(QWidget *parent = 0);
     ~About();
 
+private slots:
+    void on_label_8_linkActivated(const QString &link);
+
+    void on_label_2_linkActivated(const QString &link);
+
+    void on_label_7_linkActivated(const QString &link);
+
 private:
     Ui::About *ui;
 };
